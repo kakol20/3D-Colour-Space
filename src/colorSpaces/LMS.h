98a9974746f8d85a3dc35c7427b8be7b1ce15c40
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2024-04-05 23:14:27 +0100

    Add OkLab conversion*
    *OkLab 3D object was rotated to align with others

--- src/colorSpaces/LMS.h
+++ src/colorSpaces/LMS.h
@@ -4,43 +4,43 @@
 
 class LMS {
 public:
-	LMS(const double l, const double m, const double s);
-	LMS(const LMS& other);
-	~LMS() {};
+    LMS(const double l = 0, const double m = 0, const double s = 0);
+    LMS(const LMS& other);
+    ~LMS() {};
 
-	// ----- ASSIGNMENT OPERATOR -----
+    // ----- ASSIGNMENT OPERATOR -----
 
-	inline LMS& operator=(const LMS& other) {
-		if (this == &other) return *this;
-		m_l = other.m_l;
-		m_m = other.m_m;
-		m_s = other.m_s;
-		return *this;
-	}
+    inline LMS& operator=(const LMS& other) {
+        if (this == &other) return *this;
+        m_l = other.m_l;
+        m_m = other.m_m;
+        m_s = other.m_s;
+        return *this;
+    }
 
-	// ----- OTHER -----
+    // ----- OTHER -----
 
-	/// <summary>
-	/// Convert Linear LMS to non-linear LMS
-	/// </summary>
-	/// <param name="lms"></param>
-	/// <returns></returns>
-	static LMS LinearLMStoLMS(const LinearLMS& lms);
+    /// <summary>
+    /// Convert Linear LMS to non-linear LMS
+    /// </summary>
+    /// <param name="lms"></param>
+    /// <returns></returns>
+    static LMS LinearLMStoLMS(const LinearLMS& lms);
 
-	/// <summary>
-	/// Convert non-linear LMS to Linear LMS
-	/// </summary>
-	/// <param name="lms"></param>
-	/// <returns></returns>
-	static LinearLMS LMStoLinearLMS(const LMS& lms);
+    /// <summary>
+    /// Convert non-linear LMS to Linear LMS
+    /// </summary>
+    /// <param name="lms"></param>
+    /// <returns></returns>
+    static LinearLMS LMStoLinearLMS(const LMS& lms);
 
-	inline double GetL() const { return m_l; };
-	inline double GetM() const { return m_m; };
-	inline double GetS() const { return m_s; };
+    inline double GetL() const { return m_l; };
+    inline double GetM() const { return m_m; };
+    inline double GetS() const { return m_s; };
 
-	inline std::string Output() const { return std::to_string(m_l) + " " + std::to_string(m_m) + " " + std::to_string(m_s); };
+    inline std::string Output() const { return std::to_string(m_l) + " " + std::to_string(m_m) + " " + std::to_string(m_s); };
 
 private:
 
-	double m_l, m_m, m_s;
+    double m_l, m_m, m_s;
 };
---
+++ src/colorSpaces/OkLab.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "LMS.h"
+
+/// <summary>
+/// https://bottosson.github.io/posts/oklab/
+/// </summary>
+class OkLab {
+public:
+    OkLab(const double l = 0, const double a = 0, const double b = 0);
+    OkLab(const OkLab& other);
+    ~OkLab() {};
+
+    // ----- ASSIGNMENT OPERATOR -----
+
+    inline OkLab& operator=(const OkLab& other) {
+        if (this == &other) return *this;
+        m_l = other.m_l;
+        m_a = other.m_a;
+        m_b = other.m_b;
+        return *this;
+    }
+
+    // ----- OTHER -----
+
+    static Matrix ToOkLabMat;
+    static Matrix ToLMSMat;
+
+    inline double GetL() const { return m_l; };
+    inline double GetA() const { return m_a; };
+    inline double GetB() const { return m_b; };
+
+    inline std::string Output() const { return std::to_string(m_l) + " " + std::to_string(m_a) + " " + std::to_string(m_b); };
+
+    /// <summary>
+    /// Convert LMS to OkLab
+    /// </summary>
+    /// <param name="lms"></param>
+    /// <returns></returns>
+    static OkLab LMStoOkLab(const LMS& lms);
+
+    /// <summary>
+    /// Convert LMS
+    /// </summary>
+    /// <param name="lab"></param>
+    /// <returns></returns>
+    static LMS OkLabtoLMS(const OkLab& lab);
+
+private:
+    double m_l, m_a, m_b;
+};
